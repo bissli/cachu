f1d5ee3851217ede186a9dd798ce0c6413aad8fe
bissli <bissli@xyz.org>
2025-12-11 09:45:22 -0500

    Bump version: 0.1.0 → 0.1.1

--- src/cachu/__init__.py
+++ src/cachu/__init__.py
@@ -1,6 +1,6 @@
 """Flexible caching library with support for memory, file, and Redis backends.
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 from .backends.redis import get_redis_client
 from .config import configure, disable, enable, get_all_configs, get_config
