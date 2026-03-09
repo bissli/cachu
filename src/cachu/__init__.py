cb33b23d98ea7d06c824ce383da4d020d6bd0dc9
bissli <bissli@xyz.org>
2026-03-09 15:04:45 -0400

    Bump version: 0.3.3 → 0.3.4

--- src/cachu/__init__.py
+++ src/cachu/__init__.py
@@ -1,6 +1,6 @@
 """Flexible caching library with support for memory, file, and Redis backends.
 """
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 from .api import Backend, CacheEntry, CacheInfo, CacheMeta
 from .backends.redis import get_redis_client
